b9c102f0743a94a91ca5f36417963788812fc76a
khanhhakedo <103113411+khanhhakedo@users.noreply.github.com>
2022-04-08 16:23:52 +0700

    Extra_1

--- Assignment/Extra 1.sql
+++ Assignment/Extra 1.sql
@@ -9,38 +9,30 @@ Full_Name NVARCHAR(50) NOT NULL,
 Birth_Date DATE,
 Gender ENUM('Male', 'Female', 'Unknow'),
 ET_IQ  INT UNSIGNED NOT NULL CHECK (ET_IQ >=0 AND ET_IQ <= 20),
-ET_Gmath  INT UNSIGNED NOT NULL CHECK (ET_ET_Gmath >=0 AND ET_ET_Gmath <= 20),
+ET_Gmath  INT UNSIGNED NOT NULL CHECK (ET_Gmath >=0 AND ET_Gmath <= 20),
 ET_English INT NOT NULL CHECK (ET_English >=0 AND ET_English <= 50),
 Training_Class VARCHAR(50) NOT NULL,
-Evaluation_Notes VARCHAR (50)
+Evaluation_Notes TEXT
 );
 
 ALTER TABLE Student ADD COLUMN VTI_Account VARCHAR(50) UNIQUE NOT NULL;
 
 
 -- Exercise 2 --
-DROP DATABASE IF EXISTS Data_Type_1;
-CREATE DATABASE Data_Type_1;
-Use Data_Type_1;
-
 CREATE TABLE Type_1 (
 ID INT PRIMARY KEY AUTO_INCREMENT,
 `Name` VARCHAR(50) NOT NULL,
-`Code` 
-Modifi_Date Date NOW
+`Code` NVARCHAR(5),
+Modifi_Date DATETIME 
 );
 
 -- Exercise 3 --
-DROP DATABASE IF EXISTS Data_Type_2;
-CREATE DATABASE Data_Type_2;
-Use Data_Type_2;
-
 CREATE TABLE Type_2 (
 ID INT PRIMARY KEY AUTO_INCREMENT,
 `Name` VARCHAR(50) NOT NULL,
-Birth_Date DATE,
-Gender
-Is_Delete_Flag
+Birth_Date DATE NOT NULL,
+Gender ENUM('0','1','NULL') ,
+Is_Delete_Flag ENUM('0','1') NOT NULL 
 );
 
 
